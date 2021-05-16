aaa07b29570d1d25f8d2e661d9f342b024029142
endritberishaa <endritpb@gmail.com>
2021-05-16 13:22:46 +0200

    Mapping Profile(Found error)

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -8,6 +8,7 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<LabResult, LabResult>();
+            CreateMap<Prescription, Prescription>();
         }
     }
 }
